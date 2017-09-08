45e62a3d4c2f68c3dd8780914f2c3c2bd0c5ce74
Kevin Dahlberg <nsu_tpt@hotmail.com>
2017-09-08 18:58:34 -0500

    box showing up on screen and changing colors

--- src/components/Box.js
+++ src/components/Box.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react';
 
 export default class Box extends Component {
 
+  state = {
+    boxClass: 'box blue-box'
+  }
+
   render() {
     const { boxClass } = this.props;
 
     return (
-      <div className = "box {boxClass}"></div>
+      <div className = { boxClass }></div>
     )
-  
+
   }
 }
--- src/index.js
+++ src/index.js
@@ -3,27 +3,45 @@ import { render } from 'react-dom';
 
 import Box from './components/Box';
 
+const boxArray = [ Box, Box, Box, Box, Box ]
+const boxes = boxArray.map((box, index) =>
+    <Box key={index} />
+);
+
 class App extends Component {
 
   constructor(props) {
     super(props);
-
-    this.state = {
-      boxClass: "blue-box"
-    };
+    this.state = {boxClass: 'box blue-box'};
   }
 
-  render () {
-    return
-    (
-      <main>
-        <Box boxClass = {this.boxClass} />
-      </main>
-    )
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.changeColor(),
+      3000
+    );
+  }
+
+  changeColor() {
+    if (this.state.boxClass == 'box blue-box'){
+      this.setState({
+        boxClass: 'box red-box'
+      });
+    } else {
+      this.setState({
+        boxClass: 'box blue-box'
+      });
+    }
+  }
+
+  render() {
+    return (
+      <Box boxClass={this.state.boxClass}/>
+    );
   }
 }
 
-render (
-  <App />,
-  document.getElementById('container')
+render(
+ <App />,
+ document.getElementById('container')
 )
